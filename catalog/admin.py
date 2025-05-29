4855a681b477252752592fd3bc29c10fd32efc4c
Aminjon <aminrustamov0424@gmail.com>
2025-05-29 15:20:40 +0500

    admin.py file has been changed. Models registered.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+# admin.site.register(Author)
+
+admin.site.register(Author, AuthorAdmin)
+
+admin.site.register(Genre)
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language')
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Language)
