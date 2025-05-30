d90e39fa44c26a1415f8559f68351379dcf04a9b
Aminjon <aminrustamov0424@gmail.com>
2025-05-30 14:57:57 +0500

    Admin site has been finished.

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,10 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
 
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 # admin.site.register(Author)
+    fields = ('first_name', 'last_name', ('date_of_birth', 'date_of_death'))
+    inlines = [BookInline]
 
 admin.site.register(Author, AuthorAdmin)
 
@@ -15,15 +20,30 @@ admin.site.register(Genre)
 # admin.site.register(Book)
 # admin.site.register(BookInstance)
 
-# Register the Admin classes for Book using the decorator
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre', 'language')
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+
+    list_display = ('book', 'status', 'due_back', 'id')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 admin.site.register(Language)
