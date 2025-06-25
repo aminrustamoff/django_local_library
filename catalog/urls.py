8d036d7f8f38842ab5b08a8ff55d41d2088e1beb
Aminjon <aminrustamov0424@gmail.com>
2025-06-26 00:06:01 +0500

    book_list and book_detail are being added but not finished yet

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,7 @@ from . import views
 # The URL configuration for the catalog app.
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
 ]
 
